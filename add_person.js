5a1b6c560b15048f9d102dc02f82c1671199e59e
Avleen <avleen3@gmail.com>
2017-08-15 21:25:29 +0000

    added add_person.js which takes in the first name, last name and date of a famous person as three command line arguments and uses Knex to perform an insert.

--- add_person.js
+++ add_person.js
@@ -1,1 +1,30 @@
-add_person.js
+const pg = require("pg");
+
+const settings = require("./settings"); // settings.json
+
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : settings.hostname,
+    user : settings.user,
+    password : settings.password,
+    database : settings.database
+  }
+});
+
+let newPerson = {
+  first_name: process.argv[2],
+  last_name: process.argv[3],
+  birthdate: process.argv[4]
+};
+
+knex.insert(newPerson).into('famous_people')
+  .then(function(rows) {
+    console.log(rows);
+  })
+  .catch(function(error){
+    console.log(error);
+  })
+  .finally(function(result){
+    knex.destroy();
+})
